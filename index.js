08d8df338f3b369e4bd14b3298fbb37c31a8881b
David Anigbo <davidjachi18@gmail.com>
2023-02-28 22:01:05 +0100

    Add app files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let countEl = document.getElementById("count-el")
+let count = 0
+let saveEl = document.getElementById("save-el")
+
+
+function increment(){
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    //console.log (count)
+    let countStr  = count + " - "
+    saveEl.textContent += countStr
+}  
+
+function refresh() {
+    countEl.textContent = 0
+    count = 0
+}
